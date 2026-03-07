0785f841b9141f0a7fd9878a85edec81e02df5ba
getch-27 <getahunlast@gmail.com>
2026-03-07 03:20:27 +0300

    barchart configration

---
+++ components/charts/BarCharts.tsx
@@ -0,0 +1,126 @@
+"use client"
+
+import { TrendingUp } from "lucide-react"
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from "recharts"
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+    type ChartConfig,
+} from "@/components/ui/chart"
+
+
+export function SubCityChart({ subCityData, chartConfig }: SubCityChartProps) {
+    return (
+        <Card className="w-[50%]">
+            <CardHeader>
+                <CardTitle>Sub City Destribution</CardTitle>
+                <CardDescription>Number Participants in each Sub City</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <ChartContainer config={chartConfig}>
+                    <BarChart
+                        accessibilityLayer
+                        data={subCityData}
+                        layout="vertical"
+                        margin={{
+                            left: -20,
+                        }}
+                    >
+                        <XAxis type="number" dataKey="participants" />
+                        <YAxis
+                            dataKey="name"
+                            type="category"
+                            tickLine={true}
+                            tickMargin={0}
+                            axisLine={true}
+                            tickFormatter={(value) => value.slice(0, 8)}
+                        />
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+
+                        <Bar dataKey="participants" fill="var(--color-participants)" radius={5} >
+                            <LabelList
+                                dataKey="participants"
+                                position="right"
+                                offset={8}
+                                className="text-black"
+                                fontSize={12}
+                            />
+                        </Bar>
+                    </BarChart>
+                </ChartContainer>
+            </CardContent>
+        </Card>
+    )
+}
+
+
+
+
+export function AgeDistributionChart({ageData, chartConfig} : AgeDistributionChartProps) {
+console.log(chartConfig);
+
+  return (
+    <Card className="w-[50%]">
+      <CardHeader>
+        <CardTitle>Bar Chart - Label</CardTitle>
+        <CardDescription>January - June 2024</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            accessibilityLayer
+            data={ageData}
+            margin={{
+              top: 20,
+            }}
+            barCategoryGap={0}
+            barGap={0}
+          >
+            <CartesianGrid vertical={false} />
+             <YAxis type="number" dataKey="frequency" />
+            <XAxis
+              dataKey="range"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 5)}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+            <Bar dataKey="frequency" fill="var(--color-frequency)" radius={0}>
+              <LabelList
+                position="top"
+                offset={12}
+                className="bg-blue"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col items-start gap-2 text-sm">
+        <div className="flex gap-2 leading-none font-medium">
+          Trending up by 5.2% this month <TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+          Showing total visitors for the last 6 months
+        </div>
+      </CardFooter>
+    </Card>
+  )
+}
